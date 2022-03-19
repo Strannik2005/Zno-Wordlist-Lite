6405364749301dfeaf8595537d0bc91e1253695b
Misha Kamskov <mihapro0graming@gmail.com>
2022-03-19 08:29:18 +0200

    4 Version

--- app/src/main/java/com/example/znolite/model/CardTestItem.java
+++ app/src/main/java/com/example/znolite/model/CardTestItem.java
@@ -1,29 +1,25 @@
 package com.example.znolite.model;
 
 
+
 public class CardTestItem {
 
 
-    String id;
-    private String name;
-    String percentCorrectAnswers;
+    public int id;
+    public String name;
+    public String percentCorrectAnswers;
 
-    public CardTestItem(String name, String percentCorrectAnswers) {
-        this.name = name;
-        this.percentCorrectAnswers = percentCorrectAnswers;
-    }
 
     public CardTestItem(int id, String name, String percentCorrectAnswers) {
+        this.id = id;
         this.name = name;
         this.percentCorrectAnswers = percentCorrectAnswers;
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
+    public CardTestItem( String name, String percentCorrectAnswers) {
+        this.name = name;
+        this.percentCorrectAnswers = percentCorrectAnswers;
     }
 
     public String getName() {
